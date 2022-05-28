e4ac3eadfe4a96809a82bc0ec26f079274fb49c4
Mohamed Amer <m.amer_abutlabe@hotmail.com>
2022-05-28 04:59:50 +0200

    phase 1

--- src/Photo.js
+++ src/Photo.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const Photo = () => {
-  return <h2>photo component</h2>
-}
+const Photo = ({ urls: { regular }, alt_description }) => {
+	return (
+		<article className='photo'>
+			<img src={regular} alt={alt_description} />
+		</article>
+	);
+};
 
 export default Photo
