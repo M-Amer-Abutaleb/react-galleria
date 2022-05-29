31f039253da5fe713612ffc6e3043a9414c08dde
Mohamed Amer <m.amer_abutlabe@hotmail.com>
2022-05-29 11:13:21 +0200

    enhancing the Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 const Navbar = () => {
-  return (
-		<nav className='nav navbar container-fluid d-flex px-3 justify-content-between'>
-			<div className='nav-brand'>
-				<span className='nav-brand-span'>Galleria</span>
-			</div>
+	useEffect(() => {
+		const event = window.addEventListener('scroll', () => {
+			if (window.scrollY > 1) {
+				document.querySelector('.navbar').style.opacity = 0.6;
+				document.querySelector('.navbar').style.background = '#fff';
+				document.querySelector('.nav-brand-span').style.color = 'black';
+				document.querySelector('.nav-brand-span').style.fontSize = '1.2rem';
+			} else {
+				document.querySelector('.navbar').style.opacity = 0.9;
+				document.querySelector('.navbar').style.background = 'black';
+				document.querySelector('.nav-brand-span').style.color = 'white';
+				document.querySelector('.nav-brand-span').style.fontSize = '2.75rem';
+			}
+		});
+		return () => {
+			window.removeEventListener('scroll', event);
+		};
+	}, []);
 
-			<div className='items'>
-				<ul>
-					<li>
-						<a href='#'>Home</a>
-					</li>
-					<li>
-						<a href='#'>Products</a>
-					</li>
-					<li>
-						<a href='#'>About</a>
-					</li>
-				</ul>
+	return (
+		<nav className='nav navbar container-fluid  px-3 d-flex justify-content-center '>
+			<div className='nav-brand '>
+				<span className='nav-brand-span'>Galleria</span>
 			</div>
 		</nav>
 	);
-}
+};
 
-export default Navbar
+export default Navbar;
